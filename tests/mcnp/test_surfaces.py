f1ee5fd28bed221d6a24adb6b8b80f2d007d7959
Thomas Hyatt <thomas.hyatt.2014@live.rhul.ac.uk>
2025-04-23 15:02:09 +0100

    rcc and rhp tests update

--- tests/mcnp/test_surfaces.py
+++ tests/mcnp/test_surfaces.py
@@ -155,18 +155,109 @@ def test_SPH():
     print(surface)
 
 
-def test_RCC():
+def test_RCC1():
+    """
+    RCC about the y-axis
+    base plane at x,y,z = 0,-5,0
+    height of 10 and radius of 4
+    """
     reg = pyg4ometry.mcnp.Registry()
-    surface = pyg4ometry.mcnp.RCC(0, 0, 2, 3, 4, 0, 2, reg=reg)
+    surface = pyg4ometry.mcnp.RCC(0, -5, 0, 0, 10, 0, 4, reg=reg)
+    print(surface)
+
+
+def test_RCC2():
+    """
+    RCC rotated
+    """
+    reg = pyg4ometry.mcnp.Registry()
+    surface = pyg4ometry.mcnp.RCC(0, 0, 2, 3, 4, 0, 2, reg=reg)  # rotation
+    print(surface)
+
+
+def test_RHP_HEX1():
+    """
+    RHP (regular) about the z-axis
+    base plane is at x,y,z = 0,0,0
+    height of 8 and 1st face is normal to y-axis at y=2
+    """
+    reg = pyg4ometry.mcnp.Registry()
+    surface = pyg4ometry.mcnp.RHP_HEX(0, 0, 0, 0, 0, 8, 0, 2, 0, reg=reg)
+    print(surface)
+
+
+def test_RHP_HEX2():
+    """
+    RHP (regular) about the z-axis
+    base plane is at x,y,z = 5,5,-4
+    height of 8 and 1st face is normal to y-axis at y=2
+    """
+    reg = pyg4ometry.mcnp.Registry()
+    surface = pyg4ometry.mcnp.RHP_HEX(5, 5, -4, 0, 0, 8, 0, 2, 0, reg=reg)
+    print(surface)
+
+
+def test_RHP_HEX3():
+    """
+    RHP (regular) about the z-axis
+    base plane is at x,y,z = 0,0,0
+    height of 8 and 1st face is normal to y-axis at y=2
+    """
+    reg = pyg4ometry.mcnp.Registry()
+    surface = pyg4ometry.mcnp.RHP_HEX(0, 0, 0, 0, 8, 0, 2, 0, 0, reg=reg)
+    print(surface)
+
+
+def test_RHP_HEX4():
+    """
+    RHP (regular) rotated to be at 45 degrees between the z-axis and y-axis
+    base plane is at x,y,z = 0,0,-4
+    height of sqrt(2)*5.66=8.00 and 1st face is normal to y-axis at y=2
+    """
+    reg = pyg4ometry.mcnp.Registry()
+    surface = pyg4ometry.mcnp.RHP_HEX(0, 0, -4, 0, 5.66, 5.66, 0, 2, 0, reg=reg)
+    print(surface)
+
+
+def test_RHP_HEX5():
+    """
+    RHP (regular) rotated to be at a 22.5 degrees from the x-axis to the z-axis
+    base plan at x,y,z = 0,0,0
+    height of = [15cos(22.5), 0, 12sine(22.5)] = [13.85, 0, 5.74] = sqrt(13.85**2 + 0**2 + 5.74**2) = 14.99
+    and 1st face is normal to the x-axis at x = [6-5, 0, 0] = [1, 0, 0]
+    """
+    reg = pyg4ometry.mcnp.Registry()
+    surface = pyg4ometry.mcnp.RHP_HEX(0, 0, 0, 0, 16.98, 16.98, 0, 4, 0, reg=reg)
+    print(surface)
+
+
+def test_RHP_HEX6():
+    """
+    RHP (regular) about the z-axis
+    base plane is at x,y,z = 0,0,-4
+    height of 15 and 1st face is normal to y-axis at y=2
+    the s and t values form a regular hexagon
+    """
+    reg = pyg4ometry.mcnp.Registry()
+    surface = pyg4ometry.mcnp.RHP_HEX(
+        0, 0, -4, 0, 0, 15, 0, 2, 0, 1.732, 1, 0, -1.732, 1, 0, reg=reg
+    )
+    print(surface)
+
+
+def test_RHP_HEX7():
+    """
+    RHP (non-regular) about the z-axis
+    base plane is at x,y,z = 0,0,0
+    height of 5 and 1st face is normal to x-axis at x=2
+    the s and t values form a non-regular hexagon in the xy plane
+    """
+    reg = pyg4ometry.mcnp.Registry()
+    surface = pyg4ometry.mcnp.RHP_HEX(0, 0, 0, 0, 0, 5, 2, 0, 0, 1, 2, 0, -1, 2, 0, reg=reg)
     print(surface)
 
 
 """
-def test_RHP_HEX():
-    reg = pyg4ometry.mcnp.Registry()
-    surface = pyg4ometry.mcnp.RHP_HEX(, reg=reg)
-    print(surface)
-
 def test_REC():
     reg = pyg4ometry.mcnp.Registry()
     surface = pyg4ometry.mcnp.REC(, reg=reg)
