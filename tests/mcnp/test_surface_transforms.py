3bb5f833f18000ed4f472f15344f19e0ebeab241
Thomas Hyatt <thomas.hyatt.2014@live.rhul.ac.uk>
2025-05-21 18:46:36 +0100

    added box transform test

--- tests/mcnp/test_surface_transforms.py
+++ tests/mcnp/test_surface_transforms.py
@@ -7,13 +7,14 @@ def test_P_transform1():
     general plane with normal (1,1,1) and translation of 5
     rotated 45 degrees about the y-axis, with no translation
     """
+    theta = _np.pi / 4  # 45 degrees
     reg = pyg4ometry.mcnp.Registry()
     P = pyg4ometry.mcnp.P(0, 1, 0, 5, reg=reg)
     Pp = P.transform(
         rotation=[
-            [_np.cos(45), 0.0, _np.sin(45)],
+            [_np.cos(theta), 0.0, _np.sin(theta)],
             [0.0, 1.0, 0.0],
-            [-_np.sin(45), 0.0, _np.cos(45)],
+            [-_np.sin(theta), 0.0, _np.cos(theta)],
         ],
         translation=[0, 0, 0],
     )
@@ -24,13 +25,14 @@ def test_PX_transform1():
     """
     x-plane rotated 45 degrees about the y-axis, with no translation
     """
+    theta = _np.pi / 4  # 45 degrees
     reg = pyg4ometry.mcnp.Registry()
     PX = pyg4ometry.mcnp.PX(0, reg=reg)
     PXp = PX.transform(
         rotation=[
-            [_np.cos(45), 0.0, _np.sin(45)],
+            [_np.cos(theta), 0.0, _np.sin(theta)],
             [0.0, 1.0, 0.0],
-            [-_np.sin(45), 0.0, _np.cos(45)],
+            [-_np.sin(theta), 0.0, _np.cos(theta)],
         ],
         translation=[0, 0, 0],
     )
@@ -41,13 +43,14 @@ def test_PX_transform2():
     """
     x-plane rotated 45 degrees about the y-axis, translated 10 in the +x direction
     """
+    theta = _np.pi / 4  # 45 degrees
     reg = pyg4ometry.mcnp.Registry()
     PX = pyg4ometry.mcnp.PX(0, reg=reg)
     PXp = PX.transform(
         rotation=[
-            [_np.cos(45), 0.0, _np.sin(45)],
+            [_np.cos(theta), 0.0, _np.sin(theta)],
             [0.0, 1.0, 0.0],
-            [-_np.sin(45), 0.0, _np.cos(45)],
+            [-_np.sin(theta), 0.0, _np.cos(theta)],
         ],
         translation=[10, 0, 0],
     )
@@ -58,13 +61,14 @@ def test_PX_transform3():
     """
     x-plane rotated 45 degrees about the y-axis, translated 10 in the +x
     """
+    theta = _np.pi / 4  # 45 degrees
     reg = pyg4ometry.mcnp.Registry()
     PX = pyg4ometry.mcnp.PX(0, reg=reg)
     PXp = PX.transform(
         rotation=[
-            [_np.cos(45), 0.0, _np.sin(45)],
+            [_np.cos(theta), 0.0, _np.sin(theta)],
             [0.0, 1.0, 0.0],
-            [-_np.sin(45), 0.0, _np.cos(45)],
+            [-_np.sin(theta), 0.0, _np.cos(theta)],
         ],
         translation=[0, 10, 0],
     )
@@ -76,13 +80,14 @@ def test_PX_transform4():
     x-plane rotated 45 degrees about the y-axis,
     translated 10 in the +x and 10 in the +y direction (infinite plane so no change when doing y or z translations)
     """
+    theta = _np.pi / 4  # 45 degrees
     reg = pyg4ometry.mcnp.Registry()
     PX = pyg4ometry.mcnp.PX(0, reg=reg)
     PXp = PX.transform(
         rotation=[
-            [_np.cos(45), 0.0, _np.sin(45)],
+            [_np.cos(theta), 0.0, _np.sin(theta)],
             [0.0, 1.0, 0.0],
-            [-_np.sin(45), 0.0, _np.cos(45)],
+            [-_np.sin(theta), 0.0, _np.cos(theta)],
         ],
         translation=[10, 10, 0],
     )
@@ -91,15 +96,16 @@ def test_PX_transform4():
 
 def test_PX_transform5():
     """
-    x-plane rotated 25 degrees about the y-axis, with no translation
+    x-plane rotated 22.5 degrees about the y-axis, with no translation
     """
+    theta = _np.pi / 8  # 22.5 degrees
     reg = pyg4ometry.mcnp.Registry()
     PX = pyg4ometry.mcnp.PX(0, reg=reg)
     PXp = PX.transform(
         rotation=[
-            [_np.cos(25), 0.0, _np.sin(25)],
+            [_np.cos(theta), 0.0, _np.sin(theta)],
             [0.0, 1.0, 0.0],
-            [-_np.sin(25), 0.0, _np.cos(25)],
+            [-_np.sin(theta), 0.0, _np.cos(theta)],
         ],
         translation=[0, 0, 0],
     )
@@ -110,12 +116,13 @@ def test_PX_transform6():
     """
     x-plane rotated 25 degrees about the y-axis, with no translation
     """
+    theta = _np.pi / 8  # 22.5 degrees
     reg = pyg4ometry.mcnp.Registry()
     PX = pyg4ometry.mcnp.PX(0, reg=reg)
     PXp = PX.transform(
         rotation=[
-            [_np.cos(45), -_np.sin(45), 0.0],
-            [_np.sin(45), _np.cos(45), 0.0],
+            [_np.cos(theta), -_np.sin(theta), 0.0],
+            [_np.sin(theta), _np.cos(theta), 0.0],
             [0.0, 0.0, 1.0],
         ],
         translation=[0, 0, 0],
@@ -127,12 +134,13 @@ def test_PY_transform1():
     """
     y-plane rotated 45 degrees about the z-axis, with no translation
     """
+    theta = _np.pi / 4  # 45 degrees
     reg = pyg4ometry.mcnp.Registry()
     PY = pyg4ometry.mcnp.PY(0, reg=reg)
     PYp = PY.transform(
         rotation=[
-            [_np.cos(45), -_np.sin(45), 0.0],
-            [_np.sin(45), _np.cos(45), 0.0],
+            [_np.cos(theta), -_np.sin(theta), 0.0],
+            [_np.sin(theta), _np.cos(theta), 0.0],
             [0.0, 0.0, 1.0],
         ],
         translation=[0, 0, 0],
@@ -144,13 +152,14 @@ def test_PZ_transform1():
     """
     z-plane rotated 45 degrees about the y-axis, with no translation
     """
+    theta = _np.pi / 4  # 45 degrees
     reg = pyg4ometry.mcnp.Registry()
     PZ = pyg4ometry.mcnp.PZ(0, reg=reg)
     PZp = PZ.transform(
         rotation=[
-            [_np.cos(45), 0.0, _np.sin(45)],
+            [_np.cos(theta), 0.0, _np.sin(theta)],
             [0.0, 1.0, 0.0],
-            [-_np.sin(45), 0.0, _np.cos(45)],
+            [-_np.sin(theta), 0.0, _np.cos(theta)],
         ],
         translation=[0, 0, 0],
     )
@@ -161,14 +170,34 @@ def test_PZ_transform2():
     """
     z-plane rotated 45 degrees about the x-axis, with no translation
     """
+    theta = _np.pi / 4  # 45 degrees
     reg = pyg4ometry.mcnp.Registry()
     PZ = pyg4ometry.mcnp.PZ(0, reg=reg)
     PZp = PZ.transform(
         rotation=[
             [1.0, 0.0, 0.0],
-            [0.0, _np.cos(45), -_np.sin(45)],
-            [0.0, _np.sin(45), _np.cos(45)],
+            [0.0, _np.cos(theta), -_np.sin(theta)],
+            [0.0, _np.sin(theta), _np.cos(theta)],
         ],
         translation=[0, 0, 0],
     )
     print(PZp)
+
+
+def test_BOX_transform1():
+    """
+    box centered at the origin with 3cm sides
+    sides parallel to major axis rotated 45 degrees about the y-axis, with no translation
+    """
+    theta = _np.pi / 4  # 45 degrees
+    reg = pyg4ometry.mcnp.Registry()
+    BOX = pyg4ometry.mcnp.BOX(-2.5, -2.5, -2.5, 5, 0, 0, 0, 5, 0, 0, 0, 5, reg=reg)
+    BOXp = BOX.transform(
+        rotation=[
+            [_np.cos(theta), 0.0, _np.sin(theta)],
+            [0.0, 1.0, 0.0],
+            [-_np.sin(theta), 0.0, _np.cos(theta)],
+        ],
+        translation=[0, 0, 0],
+    )
+    print(BOXp)
