83baba38b06c11593ef1a9634e9a35419f214e5a
Thomas Hyatt <thomas.hyatt.2014@live.rhul.ac.uk>
2026-08-01 05:58:16 +0100

    vdb pytests test 3 4 add test calls

--- tests/vdb/test_vdb.py
+++ tests/vdb/test_vdb.py
@@ -1,7 +1,9 @@
 import pyg4ometry
 
 import T001_vdbBox
-import T002_vdbMultipleBoxes
+import T002_vdbBoxes
+import T003_vdbOrb
+import T004_vdbOrbs
 
 
 def test_vdbBox(tmptestdir, testdata):
@@ -18,11 +20,39 @@ def test_vdbBox(tmptestdir, testdata):
     )
 
 
-def test_vdbMultipleBoxes(tmptestdir, testdata):
-    T002_vdbMultipleBoxes.Test(
+def test_vdbBoxes(tmptestdir, testdata):
+    T002_vdbBoxes.Test(
         voxelSize=0.1,
         halfWidth=3.0,
-        fileName="multipleboxes",
+        fileName="boxes",
+        vis=False,
+        writeGDML=True,
+        writeVDB=True,
+        writeNanoVDB=True,
+        outputPath=tmptestdir,
+        testdata=testdata,
+    )
+
+
+def test_vdbOrb(tmptestdir, testdata):
+    T003_vdbOrb.Test(
+        voxelSize=0.1,
+        halfWidth=3.0,
+        fileName="orb",
+        vis=False,
+        writeGDML=True,
+        writeVDB=True,
+        writeNanoVDB=True,
+        outputPath=tmptestdir,
+        testdata=testdata,
+    )
+
+
+def test_vdbOrbs(tmptestdir, testdata):
+    T004_vdbOrbs.Test(
+        voxelSize=0.1,
+        halfWidth=3.0,
+        fileName="orbs",
         vis=False,
         writeGDML=True,
         writeVDB=True,
